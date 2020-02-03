8eb819b3ca4a780535c32cee895b24b8fe5ba36e
Farukh1x95 <farook1x95@gmail.com>
2020-02-03 14:52:14 +0530

    Change  Done

--- src/Components/repos/RepoItem.js
+++ src/Components/repos/RepoItem.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const RepoItem = ({ repo }) => {
   return (
-    <div class="card col-md-6">
-      <div class="card-body">
-        <h4 class="card-title">{repo.name}</h4>
-        <p class="card-text">{repo.name}</p>
+    <div className="card col-md-6">
+      <div className="card-body">
+        <h4 className="card-title">{repo.name}</h4>
+        <p className="card-text">{repo.name}</p>
       </div>
     </div>
   );
